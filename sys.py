8624cc18d008c98a6c7304d77dde20fc78b9724f
divyanshu <dimridd@gmail.com>
2021-04-13 00:39:24 +0530

    removed un-necessary files

--- sys.py
+++
@@ -1,8 +0,0 @@
-for epoch in range(0, 5):
-	if epoch > 1:
-		x = str(input("Enter 'Yes' to continue training for epoch[{}]: ".format(epoch)))
-		if x.lower() == "yes":
-			print("training continues.....")
-		else:
-			break
-	
